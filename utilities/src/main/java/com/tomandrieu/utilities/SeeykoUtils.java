1d89cb54f184fc8d4290bf18b46edf56d1c8284c
Tom/Seeyko <andrieutom30@gmail.com>
2020-04-30 12:28:04 +0200

    Add method openAppInGooglePlay and closeApplication

--- utilities/src/main/java/com/tomandrieu/utilities/SeeykoUtils.java
+++ utilities/src/main/java/com/tomandrieu/utilities/SeeykoUtils.java
@@ -1,6 +1,12 @@
 package com.tomandrieu.utilities;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SeeykoUtils {
     public static int pixelsInDp(int pixels, Context context) {
@@ -8,4 +14,23 @@ public class SeeykoUtils {
         return (int) (pixels * density + 0.5f);
     }
 
+    public static void openAppInGooglePlay(AppCompatActivity appCompatActivity, boolean closeApp) {
+        final String appPackageName = appCompatActivity.getPackageName(); // getPackageName() from Context or Activity object
+        try {
+            appCompatActivity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+        } catch (ActivityNotFoundException anfe) {
+            appCompatActivity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+        }
+        if (closeApp) {
+            closeApplication(appCompatActivity);
+        }
+    }
+
+    public static void closeApplication(AppCompatActivity appCompatActivity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            appCompatActivity.finishAndRemoveTask();
+        } else {
+            appCompatActivity.finishAffinity();
+        }
+    }
 }

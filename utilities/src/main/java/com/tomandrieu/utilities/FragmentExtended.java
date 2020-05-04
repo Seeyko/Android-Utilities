683407a6222ee186f8073bfbe6192ebe9b075fee
Tom/Seeyko <andrieutom30@gmail.com>
2020-05-04 12:17:28 +0200

    Add custom fragment for backpress support

---
+++ utilities/src/main/java/com/tomandrieu/utilities/FragmentExtended.java
@@ -0,0 +1,18 @@
+package com.tomandrieu.utilities;
+
+import androidx.fragment.app.Fragment;
+
+public abstract class FragmentExtended extends Fragment {
+
+    /**
+     * Handle back press in fragment
+     * Return false to fire activity back press
+     * Return true to cancel activity back press
+     *
+     * @return
+     */
+    public boolean onBackPressed() {
+        return false;
+    }
+
+}

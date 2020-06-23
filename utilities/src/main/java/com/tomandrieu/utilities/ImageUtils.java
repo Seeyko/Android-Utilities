4fe7715c3db00b122856095979f7d0b48a0720d8
Seeyko <andrieutom30@gmail.com>
2020-06-23 14:58:43 +0200

    Add getBytes()

--- utilities/src/main/java/com/tomandrieu/utilities/ImageUtils.java
+++ utilities/src/main/java/com/tomandrieu/utilities/ImageUtils.java
@@ -2,10 +2,14 @@ package com.tomandrieu.utilities;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
 
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+import java.io.ByteArrayOutputStream;
 
 public class ImageUtils {
     public final static BitmapDescriptor bitmapDescriptorFromVector(Drawable vectorDrawable) {
@@ -15,4 +19,22 @@ public class ImageUtils {
         vectorDrawable.draw(canvas);
         return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
+
+    public static byte[] getBytes(ImageView view) {
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) view.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+        return data;
+    }
+
+    public static byte[] getBytes(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
+    }
+
 }
